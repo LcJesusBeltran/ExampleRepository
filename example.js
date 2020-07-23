c9293462c3051d8844c8c3fe2c11dfa0ae1ed045
LcJesusBeltran <68673668+LcJesusBeltran@users.noreply.github.com>
2020-07-22 20:25:38 -0600

    Create example.js

---
+++ example.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo: 'Hola mundo con Vue',
+        frutas: ['Manzana','pera','otro'],
+        objetos: [
+            {nombre:'pera',cantidad:10},
+            {nombre:'manzana',cantidad:0},
+            {nombre:'platano',cantidad:20}
+        ],
+        nuevaFruta: '',
+        nuevacantidad: 0,
+        total:0
+    },
+    methods:{
+        agregarFruta: function(){
+            console.log('diste click');
+        },
+        agregafrutas(){
+            this.objetos.push({
+                nombre: this.nuevaFruta, 
+                cantidad: this.nuevacantidad
+            })
+            this.nuevaFruta = '';
+            this.nuevacantidad = 0;
+        }
+    },
+    computed:{
+        sumarFrutas(){
+            this.total = 0;
+            for(fruta of this.objetos)
+            {
+                this.total += fruta.cantidad;
+            }
+            return this.total;
+        }
+    }
+})
